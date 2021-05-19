7297154e600c92f4a8cd2ba50fce0a4d03a9d5d3
ADMIN\Admin <thachdau16t@gmail.com>
2021-05-19 19:29:50 +0700

    update

--- src/features/order/checkout-page/components/PaymentDialog.jsx
+++ src/features/order/checkout-page/components/PaymentDialog.jsx
@@ -1,8 +1,16 @@
-import React from "react";
-import {Box, Dialog, DialogTitle, ListItem, ListItemIcon, ListItemText, Typography} from "@material-ui/core";
-import {paymentConstant} from "../../../../constants/paymentConstant";
-import {CreditCard, LocalAtm} from "@material-ui/icons";
-import PayPalIcon from "../../../../asserts/icons/PayPalIcon";
+import React from 'react';
+import {
+  Box,
+  Dialog,
+  DialogTitle,
+  ListItem,
+  ListItemIcon,
+  ListItemText,
+  Typography,
+} from '@material-ui/core';
+import { paymentConstant } from '../../../../constants/paymentConstant';
+import { CreditCard, LocalAtm } from '@material-ui/icons';
+import PayPalIcon from '../../../../asserts/icons/PayPalIcon';
 
 export const mapPaymentIcon = (paymentTypes) => {
   switch (paymentTypes) {
@@ -12,10 +20,12 @@ export const mapPaymentIcon = (paymentTypes) => {
       return LocalAtm;
     case paymentConstant.PAYPAL.code:
       return PayPalIcon;
+    default:
+      return null;
   }
 };
 
-export default function PaymentDialog({open, onClose, onChange}) {
+export default function PaymentDialog({ open, onClose, onChange }) {
   const paymentTypes = Object.entries(paymentConstant).map(([k, v]) => ({
     value: v.code,
     text: v.name,
@@ -24,21 +34,23 @@ export default function PaymentDialog({open, onClose, onChange}) {
   const handleItemClick = (value) => {
     onChange(value);
     onClose();
-  }
+  };
 
   return (
     <Dialog open={open} onClose={onClose}>
       <DialogTitle disableTypography>
         <Typography variant="h4">
-          <Box textAlign="center" color="onSurface.highEmphasis">Phương thức thanh toán</Box>
+          <Box textAlign="center" color="onSurface.highEmphasis">
+            Phương thức thanh toán
+          </Box>
         </Typography>
       </DialogTitle>
-      {paymentTypes.map(({text, value}) => (
+      {paymentTypes.map(({ text, value }) => (
         <ListItem key={value} button onClick={() => handleItemClick(value)}>
           <ListItemIcon>
-            <Box component={mapPaymentIcon(value)}/>
+            <Box component={mapPaymentIcon(value)} />
           </ListItemIcon>
-          <ListItemText primary={text}/>
+          <ListItemText primary={text} />
         </ListItem>
       ))}
     </Dialog>
