8e265f16bc4e9e9a6a578756e6d8a4951ddf6bad
ADMIN\Admin <thachdau16t@gmail.com>
2021-05-20 12:38:37 +0700

    add script tag depend on merchantId

--- src/features/order/checkout-page/Checkout.jsx
+++ src/features/order/checkout-page/Checkout.jsx
@@ -1,20 +1,27 @@
-import React, {useEffect, useState} from "react";
-import {makeStyles} from "@material-ui/core/styles";
-import {Box} from "@material-ui/core";
+import React, { Fragment, useEffect, useState } from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import { Box } from '@material-ui/core';
 
-import LocationCard from "./components/LocationCard";
-import OrderDetails from "./components/OrderDetails";
-import CouponList from "./components/CouponList";
-import MainActionsBottom from "./components/MainActionsBottom";
-import TopNavigationBar from "../../common/TopNavigationBar";
-import {useHistory, useParams} from "react-router-dom";
-import {useDispatch, useSelector} from "react-redux";
-import {clearOrderState, confirmOrder, orderSelector, removeItem, setNote, setPaymentType} from "../OrderSlice";
-import {showError} from "../../common/Snackbar/SnackbarSlice";
-import AddressDialog from "./components/AddressDialog";
-import NoteDialog from "./components/NoteDialog";
-import PaymentDialog from "./components/PaymentDialog";
-
+import LocationCard from './components/LocationCard';
+import OrderDetails from './components/OrderDetails';
+import CouponList from './components/CouponList';
+import MainActionsBottom from './components/MainActionsBottom';
+import TopNavigationBar from '../../common/TopNavigationBar';
+import { useHistory, useParams } from 'react-router-dom';
+import { useDispatch, useSelector } from 'react-redux';
+import {
+  clearOrderState,
+  confirmOrder,
+  orderSelector,
+  removeItem,
+  setNote,
+  setPaymentType,
+} from '../OrderSlice';
+import { showError } from '../../common/Snackbar/SnackbarSlice';
+import AddressDialog from './components/AddressDialog';
+import NoteDialog from './components/NoteDialog';
+import PaymentDialog from './components/PaymentDialog';
+import { Helmet } from 'react-helmet';
 
 const useStyles = makeStyles((theme) => ({
   topNavigationBar: {
@@ -29,23 +36,24 @@ const useStyles = makeStyles((theme) => ({
     bottom: 0,
     left: 0,
     right: 0,
-  }
+  },
 }));
 
 export default function Checkout() {
   const classes = useStyles();
   const history = useHistory();
   const dispatch = useDispatch();
-  const {id: restaurantId} = useParams();
+  const { id: restaurantId } = useParams();
   const [addressOpen, setAddressOpen] = useState(false);
   const [paymentOpen, setPaymentOpen] = useState(false);
   const [noteOpen, setNoteOpen] = useState(false);
 
-  const {isEmpty, isError, errorMessage, data, orderSuccess} = useSelector(orderSelector);
+  const { isEmpty, isError, errorMessage, data, orderSuccess } =
+    useSelector(orderSelector);
 
   const handlePaymentTypeChange = (paymentType) => {
     dispatch(setPaymentType(paymentType));
-  }
+  };
 
   useEffect(() => {
     if (isError) {
@@ -53,7 +61,7 @@ export default function Checkout() {
       dispatch(clearOrderState());
     }
     if (orderSuccess) {
-      alert("Đặt hàng thành công!");
+      alert('Đặt hàng thành công!');
       history.replace(`/checkout/${orderId}`);
     }
   }, [isError, dispatch, orderSuccess]);
@@ -63,42 +71,74 @@ export default function Checkout() {
     return null;
   }
 
-  const {id: orderId, subTotal, delivery: {customerAddress, shippingFee}, paymentType, note} = data;
+  const {
+    id: orderId,
+    subTotal,
+    delivery: { customerAddress, shippingFee },
+    paymentType,
+    note,
+  } = data;
+
+  const merchantId = 'MERCHANT_ID';
+
+  const scriptAttributes = {
+    src: `https://www.paypal.com/sdk/js?&client-id=%REACT_APP_CLIENT_ID%&merchant-id=${merchantId}`,
+  };
 
   return (
-    <Box mt={6} mb={16.25} p={1.5}>
-      <Box className={classes.topNavigationBar}>
-        <TopNavigationBar label="Check out"/>
+    <Fragment>
+      <Helmet>
+        <script {...scriptAttributes}></script>
+      </Helmet>
+      <Box mt={6} mb={16.25} p={1.5}>
+        <Box className={classes.topNavigationBar}>
+          <TopNavigationBar label="Check out" />
+        </Box>
+        <Box>
+          <LocationCard
+            location={customerAddress}
+            handleChange={() => setAddressOpen(true)}
+          />
+        </Box>
+        <Box mt={2} mb={3}>
+          <OrderDetails
+            orderData={data}
+            handleUpdateNote={() => setNoteOpen(true)}
+            note={note}
+            handleRemoveItem={(orderItemId) => {
+              dispatch(removeItem({ orderId, orderItemId }));
+            }}
+          />
+        </Box>
+        <Box mx={-1.5}>
+          <CouponList />
+        </Box>
+        <Box className={classes.mainActionsBottom}>
+          <MainActionsBottom
+            totalCost={subTotal + shippingFee}
+            handleCheckout={() =>
+              dispatch(confirmOrder({ orderId, note, paymentType }))
+            }
+            handlePaymentChange={() => setPaymentOpen(true)}
+            disablePlaceOrder={!customerAddress}
+          />
+        </Box>
+        <AddressDialog
+          open={addressOpen}
+          onClose={() => setAddressOpen(false)}
+        />
+        <PaymentDialog
+          open={paymentOpen}
+          onClose={() => setPaymentOpen(false)}
+          onChange={(paymentType) => handlePaymentTypeChange(paymentType)}
+        />
+        <NoteDialog
+          note={note}
+          open={noteOpen}
+          onClose={() => setNoteOpen(false)}
+          onSubmit={(text) => dispatch(setNote(`${text}`))}
+        />
       </Box>
-      <Box>
-        <LocationCard location={customerAddress}
-                      handleChange={() => setAddressOpen(true)}/>
-      </Box>
-      <Box mt={2} mb={3}>
-        <OrderDetails orderData={data}
-                      handleUpdateNote={() => setNoteOpen(true)}
-                      note={note}
-                      handleRemoveItem={(orderItemId) => {
-                        dispatch(removeItem({orderId, orderItemId}));
-                      }}/>
-      </Box>
-      <Box mx={-1.5}>
-        <CouponList/>
-      </Box>
-      <Box className={classes.mainActionsBottom}>
-        <MainActionsBottom totalCost={subTotal + shippingFee}
-                           handleCheckout={() => dispatch(confirmOrder({orderId, note, paymentType}))}
-                           handlePaymentChange={() => setPaymentOpen(true)}
-                           disablePlaceOrder={!customerAddress}/>
-      </Box>
-      <AddressDialog open={addressOpen} onClose={() => setAddressOpen(false)}/>
-      <PaymentDialog open={paymentOpen}
-                     onClose={() => setPaymentOpen(false)}
-                     onChange={(paymentType) => handlePaymentTypeChange(paymentType)}/>
-      <NoteDialog note={note}
-                  open={noteOpen}
-                  onClose={() => setNoteOpen(false)}
-                  onSubmit={(text) => dispatch(setNote(`${text}`))}/>
-    </Box>
+    </Fragment>
   );
 }
