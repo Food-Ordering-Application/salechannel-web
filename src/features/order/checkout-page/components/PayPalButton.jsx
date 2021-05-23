600b5f451d1a120d4387fd4252b677e51a78461d
ADMIN\Admin <thachdau16t@gmail.com>
2021-05-23 17:48:14 +0700

    Update PayPalButton.jsx

--- src/features/order/checkout-page/components/PayPalButton.jsx
+++ src/features/order/checkout-page/components/PayPalButton.jsx
@@ -1,10 +1,10 @@
-import React, {useState} from 'react';
-import {useSelector} from 'react-redux';
-import {orderSelector} from '../../OrderSlice';
-import {paymentConstant} from '../../../../constants/paymentConstant';
-import {OrderApi} from '../../../../api/OrderApi';
-import {restaurantSelector} from "../../../restaurant/RestaurantSlice";
-import {PayPalButton} from "react-paypal-button-v2";
+import React, { useState } from 'react';
+import { useSelector } from 'react-redux';
+import { orderSelector } from '../../OrderSlice';
+import { paymentConstant } from '../../../../constants/paymentConstant';
+import { OrderApi } from '../../../../api/OrderApi';
+import { restaurantSelector } from '../../../restaurant/RestaurantSlice';
+import { PayPalButton } from 'react-paypal-button-v2';
 
 export default function PayPalButtonComponent() {
   const {
@@ -12,10 +12,12 @@ export default function PayPalButtonComponent() {
       id: orderId,
       note,
       subTotal,
-      delivery: {shippingFee},
+      delivery: { shippingFee },
     },
   } = useSelector(orderSelector);
-  const {restaurant: {merchantIdInPayPal}} = useSelector(restaurantSelector);
+  const {
+    restaurant: { merchantIdInPayPal },
+  } = useSelector(restaurantSelector);
   const [paypalOrderId, setPaypalOrderId] = useState(``);
 
   /*
@@ -41,34 +43,21 @@ export default function PayPalButtonComponent() {
    */
 
   const createOrder = function (data, actions) {
-
-    OrderApi
-      .confirmOrder(orderId, note, paymentConstant.PAYPAL.code)
-      .then(({paypalOrderId}) => {
+    OrderApi.confirmOrder(orderId, note, paymentConstant.PAYPAL.code)
+      .then(({ paypalOrderId }) => {
+        console.log(paypalOrderId);
         setPaypalOrderId(paypalOrderId);
+        return paypalOrderId;
       })
       .catch((error) => {
         console.log(error);
       });
-
-    return actions.order.create({
-      purchase_units: [{
-        amount: {
-          currency_code: "USD",
-          value: "0.01"
-        }
-      }],
-    });
   };
 
   const onApprove = function (data, actions) {
-    return actions.order.capture().then(function (details) {
-      alert("Transaction completed by " + details[`payer`][`name`][`given_name`]);
-      return OrderApi.approvePaypal(orderId, paypalOrderId)
-        .then(function (details) {
-          alert('Transaction funds captured from ' + details[`payer_given_name`]);
-        });
-    })
+    OrderApi.approvePaypal(orderId, paypalOrderId).then(function (details) {
+      alert('Transaction funds captured from ' + details[`payer_given_name`]);
+    });
   };
 
   return (
