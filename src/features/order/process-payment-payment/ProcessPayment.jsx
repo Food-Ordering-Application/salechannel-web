878083ec309b23318f80eec9d4db800be5693d38
DucHuynh99 <1712354@student.hcmus.edu.vn>
2021-07-05 23:14:37 +0700

    Update
    Test ZaloPay

--- src/features/order/process-payment-payment/ProcessPayment.jsx
+++ src/features/order/process-payment-payment/ProcessPayment.jsx
@@ -1,8 +1,8 @@
 import React, {useEffect} from "react";
 import {Box} from "@material-ui/core";
+import queryString from 'query-string'
 import TopNavigationBar from "../../common/TopNavigationBar";
-import {useHistory, useParams} from "react-router-dom";
-import {OrderApi} from "../../../api/OrderApi";
+import {useHistory, useLocation, useParams} from "react-router-dom";
 import {useDispatch} from "react-redux";
 import {showError, showSuccess} from "../../common/Snackbar/SnackbarSlice";
 
@@ -10,24 +10,23 @@ export default function ProcessPayment() {
   const {id: orderId} = useParams()
   const history = useHistory()
   const dispatch = useDispatch()
+  const {search} = useLocation()
+  const {status} = queryString.parse(search)
 
   useEffect(() => {
-    if (orderId) {
-      OrderApi.updateZaloPayment(orderId)
-        .then((data) => {
-          dispatch(showSuccess("Thanh toán thành công"))
-          history.replace(`/order/${orderId}`)
-        })
-        .catch((error) => {
-          dispatch(showError(error.message))
-        })
+    if (Number(status) === 1) {
+      dispatch(showSuccess("Thanh toán thành công"))
+      history.replace(`/order/${orderId}`)
+    } else {
+      dispatch(showError("Thanh toán thất bại. Vui lòng thử lại."))
+      dispatch(showError(`/checkout/${orderId}`))
     }
   }, [])
 
   return (
     <Box mt={6}>
       <TopNavigationBar label={"Xử lí thanh toán"} isPending={true}/>
-      <Box>{orderId}</Box>
+      <Box>{JSON.stringify(status)}</Box>
     </Box>
   )
 }
